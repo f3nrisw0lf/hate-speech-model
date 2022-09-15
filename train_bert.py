f1a75bd43950eacfb25a04085150c8a690084120
f3nrisw0lf <rodrickcorpuz@gmail.com>
2022-09-15 14:08:45 +0800

    Removed Link on Preprocessor and Encoder due to an Saved Model Error, Added Plot Visualization of by-epoch metrics

--- train_bert.py
+++ train_bert.py
@@ -1,12 +1,56 @@
+import os
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+from IPython.display import clear_output
 
 import tensorflow as tf
+from tensorflow import keras
 import tensorflow_hub as hub
 import tensorflow_text as text
 from official.nlp import optimization  # to create AdamW optimizer
+
+
+class PlotLearning(keras.callbacks.Callback):
+    """
+    Callback to plot the learning curves of the model during training.
+    """
+
+    def on_train_begin(self, logs={}):
+        self.metrics = {}
+        for metric in logs:
+            self.metrics[metric] = []
+
+    def on_epoch_end(self, epoch, logs={}):
+        # Storing metrics
+        for metric in logs:
+            if metric in self.metrics:
+                self.metrics[metric].append(logs.get(metric))
+            else:
+                self.metrics[metric] = [logs.get(metric)]
+
+        # Plotting
+        metrics = [x for x in logs if 'val' not in x]
+
+        f, axs = plt.subplots(1, len(metrics), figsize=(15, 5))
+        clear_output(wait=True)
+
+        for i, metric in enumerate(metrics):
+            axs[i].plot(range(1, epoch + 2),
+                        self.metrics[metric],
+                        label=metric)
+            if logs['val_' + metric]:
+                axs[i].plot(range(1, epoch + 2),
+                            self.metrics['val_' + metric],
+                            label='val_' + metric)
+
+            axs[i].legend()
+            axs[i].grid()
+
+        plt.tight_layout()
+        plt.show()
+
 
 tf.get_logger().setLevel('ERROR')
 
@@ -23,11 +67,13 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 print(X_train.head(4))
 
-
+# Preprocess model auto-selected: https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3
 bert_preprocess = hub.KerasLayer(
-    "https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3")
+    "bert_prereq\\bert_en_uncased_preprocess_3\\")
+
+# BERT model selected           : https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/1
 bert_encoder = hub.KerasLayer(
-    "https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/4")
+    "bert_prereq\\bert_en_uncased_L-12_H-768_A-12_4\\")
 
 # Bert layers
 text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
@@ -43,12 +89,12 @@ model = tf.keras.Model(inputs=[text_input], outputs=[l])
 
 model.compile(optimizer='adam', loss='binary_crossentropy',
               metrics=['accuracy'])
-history = model.fit(X_train, y_train, epochs=5, batch_size=32, verbose=1)
 
+history = model.fit(X_train, y_train, epochs=2,
+                    batch_size=32, verbose=1, validation_data=(X_test, y_test), callbacks=[PlotLearning()])
 
-model.save("bert")
+# model.save("bert")
 model.summary()
 
-y_predicted = model.predict(X_test)
-y_predicted = y_predicted.flatten()
-print(np.average(y_predicted))
+# y_predicted = y_predicted.flatten()
+# print(np.average(y_predicted))
